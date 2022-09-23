8a88ce9ad2fc72f823565edc147b8e9bc0176b56
ThalesDaviSouza <thalesdavisoul@gmail.com>
2022-09-23 11:51:35 -0300

    app started

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,192 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
+// Author Name: GABRIEL MACHADO VIOLANTE
+// Date: 23/09/2022
+// Turma: 303
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomeState createState() => _HomeState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _userData = "";
+  String _infoText = "Insert your data";
+  TextEditingController volumeController = TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _calculate() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      double volume = double.parse(volumeController.text);
+      String typeOfPerson = double.parse(heightController.text) / 100.0;
+
+      _infoText = "Foi";
+    });
+
+  }
+
+  void _resetFields() {
+    _formKey = GlobalKey<FormState>();
+    _userData = "";
+    volumeController.text = "";
+    setState(() {
+      _infoText = "Insert your Data";
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Caffeine Check'),
+        centerTitle: true,
+        backgroundColor: Colors.brown,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () { _resetFields(); },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.coffee_rounded,
+                  size: 120.0,
+                  color: Colors.brown,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  items: const [
+                    DropdownMenuItem(
+                      child: Text("Select who you are"),
+                      value: "",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Healthy Adult"),
+                      value: "Healthy Adult",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Children"),
+                      value: "Children",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Pregnant or Lactating"),
+                      value: "Pregnant or Lactating",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Caffeine Sensitive"),
+                      value: "Caffeine Sensitive",
+                    ),
+                  ],
+                  isExpanded: true,
+                  style: const TextStyle(color: Colors.brown, fontSize: 20),
+                  iconEnabledColor: Colors.brown,
+                  value: _userData,
+                  onChanged: (String? value) {
+                    if (value is String) {
+                      setState(() {
+                        _userData = value;
+                      });
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Ammount of Coffe (ml)',
+                        labelStyle:
+                            TextStyle(color: Colors.brown, fontSize: 20)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.brown, fontSize: 20.0),
+                    controller: volumeController,
+                    validator: (value) {
+                      if (value.toString().isEmpty) {
+                        return "Insert a valid volume";
+                      }
+
+                      if (double.parse(value.toString()) > 4000) {
+                        return "You didn't drink that much coffe";
+                      }
+
+                      if (double.parse(value.toString()) < 0) {
+                        return "You can't drink negative coffe";
+                      }
+
+                      if (num.tryParse(value.toString()) == null) {
+                        return "Insert a valid volume";
+                      }
+                    }),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: ToggleSwitch(
+                    minWidth: 100.0,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.brown!],
+                      [Colors.brown!]
+                    ],
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    totalSwitches: 2,
+                    labels: ['Espresso', 'Brewed'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      print('switched to: $index');
+                    },
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) _calculate();
+                    },
+                    child: Text('Check'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.brown,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
